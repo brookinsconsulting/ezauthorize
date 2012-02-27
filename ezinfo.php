1cae1132764dd27d49b78db9fbfc1cbe55ed3872
Brookins Consulting <info@brookinsconsulting.com>
2012-02-26 22:36:06 -0600

    Updated: Replaced info url with project url

--- ezinfo.php
+++ ezinfo.php
@@ -4,7 +4,7 @@ class ezauthorizeInfo
     function info()
     {
         return array(
-            'Name' => "<a href='http://brookinsconsulting.com/solutions/ez_authorize'>eZ Authorize</a>",
+            'Name' => "<a href='http://projects.ez.no/ezauthorize'>eZ Authorize</a>",
             'Version' => "1.4.0",
             'Copyright' => "Copyright (C) 2001 - 2007 <a href='http://brookinsconsulting.com/'>Brookins Consulting</a>",
             'Author' => "Brookins Consulting",
